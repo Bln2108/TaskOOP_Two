86c7deeee1952019a9c3632bb5567cc7df71ba41
Bln2108 <bln2108@yandex.ru>
2022-05-31 18:47:10 +0300

    Первое изменение

---
+++ src/main/java/ru/netology/oop/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.oop;
+
+public class Post {
+    public  String name; // поле для имени
+    public  String passport; // "4444 № 44444444",
+    public  String patronymic; //"Иванович",
+    public  String phone; //"+7 (999)-999-99-99",
+    public  String surname; //"Иванов",
+    public  Boolean subscription; //true
+    public FormDate birthday;
+
+}
